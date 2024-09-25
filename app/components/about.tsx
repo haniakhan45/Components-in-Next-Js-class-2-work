4239ec2cbbf410acc405d68cf8428547d0dd471c
haniakhan45 <162999510+haniakhan45@users.noreply.github.com>
2024-09-25 21:22:40 +0500

    Initial commit

---
+++ app/components/about.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hi, I'm on a journey to bring my ideas to life, driven by passion and
+        curiosity.
+      </p>
+    </div>
+  );
+}
---
+++ app/components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>Welcome to My Journey of Innovation</h1>
+      <p>I'm empowering my ideas, one step at a time.</p>
+  
+    </div>
+  );
+}
+ 
